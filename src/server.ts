1c84bb0f77a93260df73aa33cf3522edadbeaf77
shahadot99999 <shahadot99999@gmail.com>
2026-05-21 07:31:11 +0600

    Getting All Users and Single user with params

--- src/server.ts
+++ src/server.ts
@@ -44,7 +44,7 @@ app.get('/', (req : Request, res : Response) => {
 })
 
 
-app.post("/", async(req : Request, res: Response)=>{
+app.post("/api/users", async(req : Request, res: Response)=>{
     //console.log(req.body);
     // const body= req.body;
     const {name, email, password, age}= req.body;
@@ -60,16 +60,79 @@ app.post("/", async(req : Request, res: Response)=>{
       //console.log(result);
 
     res.status(201).json({
+       success: true,
         message: " User Created successfully!",
         data: result.rows[0],
     }); 
     } catch (error: any) {
       res.status(500).json({
+        success: false,
         message: error.message,
         error: error,
     });
     }
 });
+
+
+app.get('/api/users', async(req : Request, res: Response)=>{
+  try {
+    const result = await pool.query(`
+     
+      SELECT * FROM users
+      `);
+      res.status(200).json({
+        success: true,
+        message: " Users retrived successfully!",
+        data: result.rows,
+      })
+  } catch (error: any) {
+    res.status(500).json({
+        success: false,
+        message: error.message,
+        error : error,
+      })
+  }
+})
+
+
+
+app.get('/api/users/:id', async(req : Request, res : Response)=>{
+  const  {id}= req.params;
+  //console.log(id);
+  try {
+    const result = await pool.query(`
+     SELECT * FROM users WHERE id=$1 
+      `,
+      [id],
+     
+    );
+
+    
+    //console.log(result);
+    if(result.rows.length ===0){
+
+       res.status(500).json({
+        success: false,
+        message: " User Not Found!",
+        data: {},
+      })
+    }
+
+    res.status(200).json({
+        success: true,
+        message: " Users retrived successfully!",
+        data: result.rows,
+      })
+    
+  } catch (error : any) {
+     res.status(500).json({
+        success: false,
+        message: error.message,
+        error : error,
+      })
+  }
+
+})
 
 app.listen(port, () => {
   console.log(`Assignment2 NodejsExpress app listening on port ${port}`)
